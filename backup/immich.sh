dc608188db3a4aa7558e03226afb3fbbbd460001
Specht <picole95@gmail.com>
2025-02-22 11:58:20 +0100

    updated rclone-config

--- backup/immich.sh
+++ backup/immich.sh
@@ -2,4 +2,4 @@
 sudo tar -czf "$1/$2.tar.gz" "volumes/$2/library/backups"
 echo "- rclone"
 mkdir "$1/$2/"
-rclone copy "volumes/$2/library/library" "$1/$2/"
+rclone copy --config=env/rclone.conf "volumes/$2/library/library" "$1/$2/"
--- backup/owncloud.sh
+++ backup/owncloud.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "- rclone"
 mkdir "$1/$2/"
-rclone copy --config=backup/rclone.conf ocis: "$1/$2/"
+rclone copy --config=env/rclone.conf ocis: "$1/$2/"
